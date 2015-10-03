ece83f2b82ff0a30d814790ea5ed229e79255ad4
TSwizz <Tomgswartz@gmail.com>
2015-10-03 11:45:07 -0500

    removes unncessary code from ViAdjust and ValueCropRaster and adds a function to create reprojection file function

--- geoFunctions.R
+++ geoFunctions.R
@@ -1,20 +1,33 @@
-# Library of functions for converting rasters to a brick
+library(lubridate)
+library(raster)
 
+# Library of functions for converting rasters to a brick
+# doesn't work yet
+
+QuiettLoadLib <- function(libraries) {
+  lapply(libraries, require, character.only = TRUE, quietly = TRUE)
+}
+
+# converts a input (numeric or character) to a 2 day digit character for reading in file names
 
 ConvertToTwoDigits <- function(character) {
   character <- ifelse(as.numeric(character) >= 10 | nchar(character) == 2, as.character(character), paste("0", character, sep = "")) 
 }
 
-NdviAdjust <- function(data) {
-  data[data == 0] <- NA
-  data <- data *.0001
-  data <- crop(data, extent(rwaSHP))
+# makes the adjustments necessary for vegetation index
+
+ViAdjust <- function(data) {
+  data[data == -3000] <- NA # -3000 is the NA value for MODIS
+  data <- data *.0001 # data is stored multipled by 10000
 }
+
+# takes a point's lat and lon and makes a bounding box around it up to a distance in degrees
+# specified by the user
 
 MakePointExtent <- function(pointFile, rowName, point, degreeDistance) {
   csv <- read.csv(pointFile)
-  lon <- csv[csv$plantationName == "Mulindi", ]$longitude
-  lat <- csv[csv$plantationName == "Mulindi", ]$latitude
+  lon <- csv[csv[, rowName] == point, ]$longitude
+  lat <- csv[csv[, rowName] == point, ]$latitude
   upperLon <- lon + degreeDistance
   lowerLon <- lon - degreeDistance
   upperLat <- lat + degreeDistance
@@ -28,9 +41,51 @@ MakePointExtent <- function(pointFile, rowName, point, degreeDistance) {
 # the output raster has the extent set and the non-crop cells are made to be NA
 # option to set the resample type, defaults to bilinear with nearest neighbor also an option
 
-ValueCropRaster <- function(valueRaster, coverRaster, resample = "bilinear") {
-  valueRaster <- resample(valueRaster, coverRaster, resample)
-  #   coverRaster[coverRaster != 3] <- NA # sets all non cropland cells to NA
-  #   coverRaster[coverRaster == 3] <- 1 # sets all cropland cells to 1 for multiplication
-  valueRaster <- valueRaster * coverRaster
+ValueCropRaster <- function(valueRaster, coverRaster, resampleMethod = "bilinear") {
+  valueRaster <- resample(valueRaster, coverRaster, resampleMethod)
+  valueRaster <- mask(valueRaster, coverRaster)
+}
+
+ReprojectParam <- function(input, output, paramFile) {
+  print(paramFile)
+  sink(file = paramFile)
+  cat(paste("INPUT_FILENAME = ", input, sep = ""))
+  cat("\n")
+  cat("\n")
+  cat("SPECTRAL_SUBSET = ( 1 )")
+  cat("\n")
+  cat("\n")
+  cat("SPATIAL_SUBSET_TYPE = INPUT_LAT_LONG")
+  cat("\n")
+  cat("\n")
+  cat("SPATIAL_SUBSET_UL_CORNER = ( 0.0 19.999999998 )")
+  cat("\n")
+  cat("SPATIAL_SUBSET_LR_CORNER = ( -9.999999999 40.617064472 )")
+  cat("\n")
+  cat("\n")
+  cat(paste("OUTPUT_FILENAME = ", output, sep = ""))
+  cat("\n")
+  cat("\n")
+  cat("RESAMPLING_TYPE = NEAREST_NEIGHBOR")
+  cat("\n")
+  cat("\n")
+  cat("OUTPUT_PROJECTION_TYPE = UTM")
+  cat("\n")
+  cat("\n")
+  cat("OUTPUT_PROJECTION_PARAMETERS = ( 
+       0.0 0.0 0.0
+       0.0 0.0 0.0
+       0.0 0.0 0.0
+       0.0 0.0 0.0
+       0.0 0.0 0.0 )
+       ")
+  cat("\n")
+  cat("\n")
+  cat("DATUM = WGS84")
+  cat("\n")
+  cat("\n")
+  cat("UTM_ZONE = 0")
+  cat("\n") # This last one is important since it won't work properly 
+            # without it
+  sink()
 }
