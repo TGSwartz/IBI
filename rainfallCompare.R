9cdf465e9a215650f3cbd00f178ac37f2e92f8f8
TSwizz <Tomgswartz@gmail.com>
2015-10-18 15:01:20 -0500

    add in ARC compatibability and also make moving averages for temperatures and evi (not plotted)

--- rainfallCompare.R
+++ rainfallCompare.R
@@ -1,6 +1,6 @@
 # compare rainfall data from TAMSAT, CHIRPS, and Mulindi's Plantation Data
 
-libraries <- c("data.table", "plyr", "TTR", "corrplot", "matrixStats")
+libraries <- c("data.table", "plyr", "TTR", "matrixStats", "corrplot", "zoo", "ggplot2")
 lapply(libraries, require, character.only = TRUE, quietly = TRUE)
 
 source("/Users/Tom/Github/IBI/bimonthClean.R")
@@ -8,7 +8,7 @@ source("/Users/Tom/Github/IBI/bimonthClean.R")
 # load the different datasets and keep rainfall estimates, dates (for mergining), and 
 # recorded yield for correlation analysis
 
-mulindi <- subset(mulindi, select = c("yield", "date", "rainfall"))
+mulindi <- subset(mulindi, select = c("yield", "date", "rainfall", "tMax", "tMin", "evi"))
 mulindi <- rename(mulindi, c("rainfall" = "plantationRainfall"))
 
 tamsat <- read.csv("/Users/Tom/Documents/IBI/tamsat.csv")
@@ -17,18 +17,22 @@ tamsat$date <- as.Date(tamsat$date)
 chirps <- read.csv("/Users/Tom/Documents/IBI/chirps.csv")
 chirps <- rename(subset(chirps, select = c("date", "rain")), c( "rain" = "chirpsRainfall"))
 chirps$date <- as.Date(chirps$date)
+arc <- read.csv("/Users/Tom/Documents/IBI/arc.csv")
+arc <- rename(subset(arc, select = c("date", "rain")), c("rain" = "arcRainfall"))
+arc$date <- as.Date(arc$date)
 
 # merge the datasets and create median and mean averages
 
-rainDf <- merge(merge(mulindi, tamsat, by = "date", all = T),
-                chirps, by = "date", all = F)
-avgCol <- c("plantationRainfall", "tamsatRainfall", "chirpsRainfall")
+rainDf <- merge(merge(merge(mulindi, tamsat, by = "date", all.x = T, all.y = F),
+                chirps, by = "date", all.x = T, all.y = F), arc, by = "date", all.x = T, all.y = F)
+#avgCol <- c("plantationRainfall", "tamsatRainfall", "chirpsRainfall", "arcRainfall")
+avgCol <- c("tamsatRainfall", "chirpsRainfall", "arcRainfall")
 rainDf$medianRainfall <- rowMedians(as.matrix(rainDf[, avgCol]))
 rainDf$meanRainfall <- rowMeans(rainDf[, avgCol])
 
 # create a correlation plots (excluding yield)
 
-corCol <- c("plantationRainfall", "tamsatRainfall", "chirpsRainfall", "meanRainfall", "medianRainfall")
+corCol <- c("plantationRainfall", "tamsatRainfall", "chirpsRainfall", "arcRainfall", "meanRainfall", "medianRainfall")
 customCol <- colorRampPalette(c("red", "grey", "blue"))(100)
 jpeg("/Users/Tom/Documents/IBI/rainCorrplot.jpg")
 corrplot(cor(rainDf[, corCol], use= "complete.obs"), method = "number", diag = T, 
@@ -39,9 +43,9 @@ dev.off()
 #                        tamsatSMA = rep(NA, nrow(rainDf)),
 #                        chirpsSMA = rep(NA, nrow(rainDf))) 
 
-rainDf <- rainDf[!is.na(rainDf$plantationRainfall), ]
+#rainDf <- rainDf[!is.na(rainDf$plantationRainfall), ]
 
-tamsatSMA <- matrix(nrow = nrow(rainDf), ncol = 180)
+tamsatSMA <- matrix(nrow = nrow(rainDf), ncol = 100)
 tamsatCor <- rep(NA, ncol(tamsatSMA))
 plantationSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
 plantationCor <- rep(NA, ncol(tamsatSMA))
@@ -51,38 +55,55 @@ medianSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
 medianCor <- rep(NA, ncol(tamsatSMA))
 meanSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
 meanCor <- rep(NA, ncol(tamsatSMA))
+tMaxSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
+tMaxCor <- rep(NA, ncol(tamsatSMA))
+tMinSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
+tMinCor <- rep(NA, ncol(tamsatSMA))
+arcSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
+arcCor <- rep(NA, ncol(tamsatSMA))
+eviSMA <- matrix(nrow = nrow(rainDf), ncol = ncol(tamsatSMA))
+eviCor <- rep(NA, ncol(tamsatSMA))
 
 #tamsamSMA <- apply(rainDf$tamsatRainfall, MARGIN = 1, FUN = function(x) SMA(x, ))
 
 # Find a way to improve with apply
 
 for (i in 1:ncol(tamsatSMA)) {
-  tamsatSMA[, i] <- SMA(rainDf$tamsatRainfall, n = i)
+  tamsatSMA[, i] <- rollapply(rainDf$tamsatRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
   tamsatCor[i] <- cor(tamsatSMA[, i], rainDf$yield, use = "complete.obs")
-  plantationSMA[, i] <- SMA(rainDf$plantationRainfall, n = i)
+  plantationSMA[, i] <- rollapply(rainDf$plantationRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
   plantationCor[i] <- cor(plantationSMA[, i], rainDf$yield, use = "complete.obs")
-  chirpsSMA[, i] <- SMA(rainDf$chirpsRainfall, n = i)
+  chirpsSMA[, i] <- rollapply(rainDf$chirpsRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
   chirpsCor[i] <- cor(chirpsSMA[, i], rainDf$yield, use = "complete.obs")
-  medianSMA[, i] <- SMA(rainDf$medianRainfall, n = i)
+  arcSMA[, i] <- rollapply(rainDf$arcRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
+  arcCor[i] <- cor(arcSMA[, i], rainDf$yield, use = "complete.obs")
+  medianSMA[, i] <- rollapply(rainDf$medianRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
   medianCor[i] <- cor(medianSMA[, i], rainDf$yield, use = "complete.obs")
-  meanSMA[, i] <- SMA(rainDf$meanRainfall, n = i)
+  meanSMA[, i] <- rollapply(rainDf$meanRainfall, width = i, mean, na.rm = T, fill = NA, align = "right")
   meanCor[i] <- cor(meanSMA[, i], rainDf$yield, use = "complete.obs")
+  tMaxSMA[, i] <- rollapply(rainDf$tMax, width = i, mean, na.rm = T, fill = NA, align = "right")
+  tMaxCor[i] <- cor(tMaxSMA[, i], rainDf$yield, use = "complete.obs")
+  tMinSMA[, i] <- rollapply(rainDf$tMin, width = i, mean, na.rm = T, fill = NA, align = "right")
+  tMinCor[i] <- cor(tMinSMA[, i], rainDf$yield, use = "complete.obs")
+  eviSMA[, i] <- rollapply(rainDf$evi, width = i, mean, na.rm = T, fill = NA, align = "right")
+  eviCor[i] <- cor(eviSMA[, i], rainDf$yield, use = "complete.obs")
 }
 
 smaDf <- data.frame(index = seq(1, ncol(tamsatSMA)), 
-                    Plantation = plantationCor, TAMSAT = tamsatCor, CHIRPS = chirpsCor,
+                    Plantation = plantationCor, TAMSAT = tamsatCor, CHIRPS = chirpsCor, ARC = arcCor,
                     Median = medianCor, Mean = meanCor)
 
 smaPlot <- ggplot(smaDf, aes(index)) + 
   geom_line(aes(y = Plantation, colour = "Plantation")) + 
   geom_line(aes(y = TAMSAT, colour = "TAMSAT")) +
   geom_line(aes(y = CHIRPS, colour = "CHIRPS")) +
+  geom_line(aes(y = ARC, colour = "ARC")) +
   geom_line(aes(y = Median, colour = "Median")) +
   geom_line(aes(y = Mean, colour = "Mean")) +
   ggtitle("Correlation Of Rainfall Estimate Moving Averages with Yield") +
   xlab("Number of Days in Moving Average") + ylab("Correlation Coefficient") +
   scale_colour_discrete(name  ="Rainfall Product", 
-                        breaks = c("Plantation", "TAMSAT", "CHIRPS", "Mean", "Median"))
+                        breaks = c("Plantation", "TAMSAT", "CHIRPS", "ARC", "Mean", "Median"))
 print(smaPlot)  
 
 #print(qplot(seq(1, length(tamsatCor), by = 1), tamsatCor))
