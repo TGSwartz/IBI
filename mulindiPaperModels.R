e6d5a131ffe824f747a42729c59d22c0fc0d3415
TSwizz <Tomgswartz@gmail.com>
2015-12-05 11:38:08 -0800

    adds some extra code for a neural net

--- mulindiPaperModels.R
+++ mulindiPaperModels.R
@@ -2,7 +2,6 @@ library(caret)
 library(doMC)
 registerDoMC(2)
 
-
 #mulindi <- subset(mulindi, select = -c(cumulativeDays))
 
 #trainControl <- trainControl(method = "repeatedcv", number = 5, repeats = 10)
@@ -72,7 +71,7 @@ satCompCaseLMFit <- train(yield ~ ., data = satDta[complete.cases(plantDta), ], 
 print(satCompCaseLMFit)
 
 
-timeOnlyCol <- c("month", "weekday", "yield")
+timeOnlyCol <- c("month", "weekday", "week", "yield")
 
 set.seed(1)
 fullTimeOnlyRFFit <- train(yield ~ ., data = subset(mulindi, select = timeOnlyCol), method = "rf", 
@@ -91,24 +90,72 @@ fullTimeOnlyLMFit <- train(yield ~ ., data = subset(mulindi, select = timeOnlyCo
 print(fullTimeOnlyLMFit)
 
 resamps <- resamples(list(fullRFFit = fullRFFit,
-                          fullGLMFit = fullGLMFit,
+                          fullEnetFit = fullGLMFit,
                           fullLMFit = fullLMFit,
                           plantRFFit = plantRFFit,
-                          plantGLMFit = plantGLMFit,
+                          plantEnetFit = plantGLMFit,
                           plantLMFit = plantLMFit,
                           satRFFit = satRFFit,
-                          satGLMFit = satGLMFit,
+                          satEnetFit = satGLMFit,
                           satLMFit = satLMFit,
                           satCompCaseRFFit = satCompCaseRFFit,
-                          satCompCaseGLMFit = satCompCaseGLMFit,
+                          satCompCaseEnetFit = satCompCaseGLMFit,
                           satCompCaseLMFit = satCompCaseLMFit,
                           fullTimeOnlyRFFit = fullTimeOnlyRFFit,
-                          fullTimeOnlyGLMFit = fullTimeOnlyGLMFit,
-                          fullTimeOnlyLMFit = fullTimeOnlyLMFit))
+                          fullTimeOnlyEnetFit = fullTimeOnlyGLMFit,
+                          fullTimeOnlyLMFit = fullTimeOnlyLMFit),
+                     modelNames = c("Full Random Forest", "Full Elastic Net", "Full OLS",
+                                    "Estate-Only Random Forest", "Estate-Only Elastic Net", "Estate-Only OLS",
+                                    "Satellite-Only Random Forest", "Satellite-Only Elastic Net", "Satellite-Only OLS",
+                                    "Satellite Complete Observations Random Forest", "Satellite Complete Observations Elastic Net", "Satellite Complete Obs OLS",
+                                    "Time Variables-Only Random Forest", "Time Variables-Only Elastic Net", "Time Variables-Only OLS"))
 
 save(resamps, file = "/Users/Tom/Documents/IBI/modelResamples.RData")
 
+set.seed(1)
+satWeekRFFit <- train(yield ~ ., data = satWeekDta, method = "rf", 
+                      trControl = trainControl, tuneLength = 10, metric = "Rsquared", 
+                      ntree = 1000)
+
+set.seed(1)
+satWeekGLMFit <- train(yield ~ ., data = satWeekDta, method = "glmnet", 
+                       trControl = trainControl, tuneLength = 10, 
+                       preProcess = c("center", "scale"), 
+                       metric = "Rsquared")
+
+set.seed(1)
+satMonthRFFit <- train(yield ~ ., data = satMonthDta, method = "rf", 
+                       trControl = trainControl, tuneLength = 10, metric = "Rsquared", 
+                       ntree = 1000)
+
+set.seed(1)
+satMonthGLMFit <- train(yield ~ ., data = satMonthDta, method = "glmnet", 
+                        trControl = trainControl, tuneLength = 10, 
+                        preProcess = c("center", "scale"), metric = "Rsquared")
+
+timeResamps <- resamples(list(satWeekRFFit = satWeekRFFit,
+                              satWeekEnetFit = satWeekGLMFit,
+                              satMonthRFFit = satMonthRFFit,
+                              satMonthEnetFit = satMonthGLMFit),
+                         modelNames = c("Satellite Weekly Random Forest", 
+                                        "Satellite Weekly Elastic Net", 
+                                        "Satellite Monthly Random Forest", 
+                                        "Satellite Monthly Elastic Net"))
+
 saveList <- c("fullRFFit", "fullGLMFit", "fullLMFit", "plantRFFit", "plantGLMFit", "plantLMFit", 
-              "satRFFit", "satGLMFit", "satLMFit", "satCompCaseRFFit", "satCompCaseGLMFit", 
-              "satCompCaseLMFit", "fullTimeOnlyRFFit", "fullTimeOnlyGLMFit", "fullTimeOnlyLMFit")
+               "satRFFit", "satGLMFit", "satLMFit", "satCompCaseRFFit", "satCompCaseGLMFit", 
+               "satCompCaseLMFit", "fullTimeOnlyRFFit", "fullTimeOnlyGLMFit", "fullTimeOnlyLMFit",
+               "satWeekRFFit", "satWeekGLMFit", "satMonthRFFit", "satMonthGLMFit")
 save(list = saveList, file = "/Users/Tom/Documents/IBI/mulindiModels.RData")
+
+# mulindiX <- subset(mulindi[!is.na(mulindi$yield),], select = -c(yield, month, weekday, week, weekend))
+# tooHighCor <- findCorrelation(cor(mulindiX, use = "complete.obs"), cutoff = .75)
+# mulindiLowCor <- mulindiX[, -tooHighCor]
+# preProc <- preProcess(mulindiLowCor, method = c("center", "scale"))
+# mulindiLowCor <- predict(preProc, mulindiLowCor)
+# mulindiLowCor <- cbind(mulindiLowCor, subset(mulindi[!is.na(mulindi$yield),], select = c(month, weekday, week, weekend, yield)))
+# #mulindiLowCor$yield <- mulindi$yield
+# 
+# nnetFit <- avNNet(mulindiLowCor, mulindiLowCor$yield, 
+#                   linout = T, trace = F, maxit = 500, size = 5, decay = .1,
+#                   maxNWts = 10 * (ncol(mulindiLowCor) + 1) + 10 + 1)
